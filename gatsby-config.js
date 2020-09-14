7d13512dcc9c6b8d0ce252ff981841feeda264ea
dawsonbooth <github@dawsonbooth.com>
2020-09-14 16:15:14 -0500

    Deploy with gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
+  pathPrefix: "/dawsonbooth.github.io",
   siteMetadata: {
     title: `Personal Website`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
-    `gatsby-plugin-theme-ui`
+    `gatsby-plugin-theme-ui`,
   ],
 };
